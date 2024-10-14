2818bf1d8f9f02a4ffa7418a253751f267e30f6b
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-14 22:28:09 +0300

    referanslar bbitti

--- src/pages/Kurumsal.jsx
+++ src/pages/Kurumsal.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { referans } from '../helper/referans'
 
 const Kurumsal = () => {
   return (
-    <div>Kurumsal</div>
+    <div>
+
+    <div className='h-25 d-flex justify-content-center gap-3 m-5 pt-5 '>
+
+    <div className="card  text-center w-25">
+  <div className="card-body ">
+      <h1 className='text-primary'><i class="fa-solid fa-users"></i></h1>
+       <p>20.00 +</p>
+       <p>Müşteri</p>
+  </div>
+</div>
+<div className="card  text-center w-25">
+  <div className="card-body">
+      <h1 className='text-secondary'><i class="fa-solid fa-globe"></i></h1>
+       <p>3 +</p>
+       <p>Ülkede Ofis</p>
+  </div>
+</div>
+<div className="card text-center w-25">
+  <div className="card-body">
+      <h1 className='ext-danger-emphasis'><i class="fa-solid fa-briefcase"></i></h1>
+        <p>12 +</p>
+       <p>Satış Yapılan Ülke</p>
+  </div>
+</div>
+
+    </div>
+
+
+    <div className='ref'>
+{referans.map((ar)=>{
+  const {id, image} = ar
+  return(
+    <div className="ref-card " style={{ width: "12rem" }} key={id}>
+  <img src={image} className="card-img-top" alt="..." />
+
+</div>
+
+  )
+})}
+
+</div>
+
+
+
+    
+    </div>
   )
 }
 
