1a3dbc8d2dd43c6475d956b21ce9cc6f0aeed34e
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-15 23:27:30 +0300

    kullandığımız teknolojiler

---
+++ src/helper/yazilim.js
@@ -0,0 +1,96 @@
+export const webtasarim =[
+    {
+      id:1,
+      title:"İhtiyaç Analizi ve Planlama",
+      text:"Başarılı bir web tasarımının ilk adımı, işletmenizin hedeflerini ve kullanıcı beklentilerini anlamaktan geçer. Bu aşamada:",
+      li1:"Hedef kitleniz analiz edilir.",
+      li2:"Rakip siteler incelenir.",
+      li3:"Web sitesinin amacı ve fonksiyonları netleştirilir.",
+      li4:"Proje süreci detaylandırılır.",
+      text2:"Bu analiz, tasarım aşamasında atılacak adımları şekillendirir ve projenin sorunsuz ilerlemesini sağlar.",
+   
+   
+     },
+    {
+      id:2,
+      title:" Site Haritası ve Prototip Oluşturma",
+      text:"Planlama aşamasından sonra, web sitenizin mimarisi ve kullanıcı deneyimi üzerinde çalışırız. Bu aşamada:",
+      li1:"Site haritası oluşturularak sayfalar arasında nasıl bir hiyerarşi kurulacağı belirlenir.",
+      li2:"Wireframe ve prototipler oluşturularak tasarımın ilk aşaması somut hale getirilir.",
+      text2:"Kullanıcı dostu bir deneyim sunmak için, ziyaretçilerin sitenizi kolayca gezinebilmelerini sağlayan bir yapı tasarlarız.",
+   
+   
+     },
+    {
+      id:3,
+      title:"Görsel Tasarım (UI/UX)",
+      text:"Markanızın kimliğini yansıtan ve ziyaretçilerin ilgisini çekecek bir web tasarımı oluştururuz. Tasarım aşamasında:",
+      li1:"Renk paletleri, yazı tipleri ve görseller seçilir.",
+      li2:"Web sitesinin genel stil ve estetiği şekillendirilir.",
+      li3:"Kullanıcı deneyimi (UX) odaklı bir tasarım stratejisi geliştirilir.",
+      text2:"Web siteniz, hem estetik açıdan etkileyici hem de kullanıcılar için rahat bir gezinti deneyimi sunacak şekilde tasarlanır.",
+   
+   
+     },
+    {
+      id:4,
+      title:" Geliştirme (Kodlama)",
+      text:"Tasarım aşaması tamamlandıktan sonra, sitenizi dinamik ve fonksiyonel hale getirmek için geliştirme aşamasına geçilir. Bu aşamada:",
+      li1:"Ön yüz geliştirme (Frontend)",
+      li2:"Arka yüz geliştirme (Backend)",
+      text2:"Web siteniz, hem masaüstü hem de mobil cihazlarda mükemmel bir deneyim sunacak şekilde optimize edilir.",
+   
+   
+     },
+    {
+      id:5,
+      title:"Test ve Optimizasyon",
+      text:"Web sitesi tamamlandıktan sonra her detayını titizlikle test ederiz. Bu aşamada:",
+      li1:"Tarayıcı uyumluluğu ve mobil cihaz performansı test edilir.",
+      li2:"Fonksiyonel testler ile form, link ve diğer etkileşimler kontrol edilir.",
+      li3:"Kullanıcı deneyimi test edilerek, web sitesinin hız ve kullanılabilirliği optimize edilir.",
+      text2:"Herhangi bir hata veya aksaklık olmadan sitenizin sorunsuz çalıştığından emin oluruz.",
+   
+   
+     },
+    {
+      id:6,
+      title:"Yayına Alma (Launch)",
+      text:"Web sitesi tamamlandıktan sonra her detayını titizlikle test ederiz. Bu aşamada:",
+      li1:"Alan adı ve hosting ayarları yapılır.",
+      li2:"Web sitesi, canlı sunucuya yüklenir ve yayınlanır.",
+      li3:"SEO uyumlu içerik ve teknik optimizasyonlarla sitenizin arama motorlarında görünürlüğü artırılır.",
+      text2:"Markanızı dijital dünyada başarıyla temsil eden web sitenizi, güçlü bir başlangıç yapmanız için yayına alıyoruz.",
+   
+   
+     },
+    {
+      id:7,
+      title:"Bakım ve Güncellemeler",
+      text:"Web siteniz yayına alındıktan sonra da yanınızdayız. Sürekli güncel ve performanslı kalması için:",
+      li1:"Güvenlik güncellemeleri yapılır.",
+      li2:"İçerik yönetimi ve yeni özellik eklemeleri yapılır.",
+      li3:"Performans ve SEO izlemeleri düzenli olarak gerçekleştirilir.",
+      text2:"Web siteniz her zaman güncel, güvenli ve hızlı kalacak şekilde düzenli bakım hizmetleri sunarız.",
+     },
+
+
+  ];
+
+  export const tekno= [
+    {
+      id: 1,
+      title: 'Html 5',
+      description: 'Özel hazırlanmış kurumsal siteleri için esnek ve kullanımı kolay olan WordPress platformunu kullanıyoruz. WordPress’in çekirdek altyapısına özel entegrasyonlar sağlıyoruz ve müşteri ihtiyaçlarına özel çözümler geliştiriyoruz.'
+    },
+    {
+      id: 2,
+      title: 'CSS',
+      description: 'PHP ile güçlü ve güvenilir web uygulamaları oluşturuyoruz. Laravel gibi modern çerçeveleri kullanarak, projelerinizi hızlıca geliştiriyoruz.'
+    },
+    {
+      id: 3,
+      title: 'Vue.js',
+      description: 'Modern ve dinamik kullanıcı arayüzleri oluşturmak için Vue.js çerçevesini kullanıyoruz. Bu sayede projelerinizi daha interaktif hale getiriyoruz.'
+    },
+  ];
--- src/pages/WebTasarim.jsx
+++ src/pages/WebTasarim.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import web from "../assets/webtasarım.jpg"
 import "../css/web.css"
 import Card from 'react-bootstrap/Card';
+import { Carousel } from 'react-bootstrap';
+import { tekno, webtasarim } from '../helper/yazilim';
+
 
 
 const WebTasarım = () => {
@@ -13,32 +16,54 @@ const WebTasarım = () => {
     <div className='web-tas-giris'>
       <h4>İnternet dünyasında markanızın güçlü bir izlenim bırakmasını sağlayacak bir web sitesi tasarlamak, uzmanlık ve stratejik bir süreç gerektirir. Romsis olarak, işletmenizin ihtiyaçlarına ve hedef kitlenizin beklentilerine uygun, modern ve etkileyici web siteleri tasarlıyoruz.</h4>
     </div>
-    <div>
+    <div className='web-tas-surec-title'>
       <h2>Web sitenizi tasarlarken izleyeceğimiz süreç:</h2>
     </div>
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      
-      </Card.Body>
-    </Card>
+    <div className='web-tas-surec d-flex flex-wrap gap-5 justify-content-center'>
+  {webtasarim.map((tasarim) => {
+    const { id, text, text2, title, li1, li2, li3, li4 } = tasarim;
+    return (
+      <Card style={{ width: '19rem' }} key={id}>
+        <Card.Body>
+          <Card.Title>{id}.</Card.Title>
+          <Card.Title className="mb-2 text-muted">{title}</Card.Title>
+          <Card.Text>{text}</Card.Text>
+
+          <ul>
+            {li1 && <li>{li1}</li>}
+            {li2 && <li>{li2}</li>}
+            {li3 && <li>{li3}</li>}
+            {li4 && <li>{li4}</li>}
+          </ul>
+
+          <Card.Text>{text2}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  })}
+</div>
+    <div className='web-tas-surec-title'>
+      <h2>Hangi Teknolojileri Kullanıyoruz</h2>
+    </div>
+    <div>
+    <Carousel>
+      {tekno.map((tech) => (
+        <Carousel.Item key={tech.id}>
+       
+          <div className="card p-4 text-center">
+          <img src="" alt="" />
+            <h3>{tech.title}</h3>
+            <p>{tech.description}</p>
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+
+    </div>
+
+   
+
+
 
 
 
