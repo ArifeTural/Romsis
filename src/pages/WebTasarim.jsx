6099c98fd5a2ddbb7281c58f46c10a2943c9cc95
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-17 02:42:22 +0300

    düzenleme

--- src/pages/WebTasarim.jsx
+++ src/pages/WebTasarim.jsx
@@ -1,96 +1,133 @@
-import React from 'react'
-import web from "../assets/webtasarım.jpg"
-import "../css/web.css"
+import React, { useEffect, useState } from 'react';
+import web from "../assets/webtasarım.jpg";
+import "../css/web.css";
 import Card from 'react-bootstrap/Card';
 import { Carousel } from 'react-bootstrap';
-import { tekno, teknoloji, webtasarim } from '../helper/yazilim';
+import { tekno,  webtasarim } from '../helper/yazilim';
+import { gsap } from 'gsap'; // GSAP importunu ekleyin
 
 
 
-const WebTasarım = () => {
+const WebTasarim = () => {
+  useEffect(() => {
+    const rotateRing = gsap.timeline({ repeat: -1 });
+    rotateRing.to('.ring-container', {
+      rotateY: 360,
+      duration: 8,
+      ease: 'none',
+    });
+  }, []);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setActiveIndex(selectedIndex);
+  };
+
   return (
     <div>
-    <div className='w-100 d-flex justify-content-center'>
-    <img src={web} alt="" className='web-tasarim-img'  />
-    </div>
-    <div className='web-tas-giris m-5 p-5 text-center'>
-      <h4 className='fs-2'>İnternet dünyasında markanızın güçlü bir izlenim bırakmasını sağlayacak bir web sitesi tasarlamak, uzmanlık ve stratejik bir süreç gerektirir. Romsis olarak, işletmenizin ihtiyaçlarına ve hedef kitlenizin beklentilerine uygun, modern ve etkileyici web siteleri tasarlıyoruz.</h4>
-    </div>
-    <div className='web-tas-surec-title m-5 pt-4'>
-      <h2 className='fs-1'>Web sitenizi tasarlarken izleyeceğimiz süreç:</h2>
-    </div>
-    <div className='web-tas-surec d-flex flex-wrap gap-5 justify-content-center '>
-  {webtasarim.map((tasarim) => {
-    const { id, text, text2, title, li1, li2, li3, li4 } = tasarim;
-    return (
-      <Card style={{ width: '19rem' }} key={id} className=' web-tas-surec border-0'>
-        <Card.Body>
-          <Card.Title className='fs-1'>{id}.</Card.Title>
-          <Card.Title className="mb-2  fs-2">{title}</Card.Title>
-          <Card.Text className=''>{text}</Card.Text>
-
-          <ul>
-            {li1 && <li>{li1}</li>}
-            {li2 && <li>{li2}</li>}
-            {li3 && <li>{li3}</li>}
-            {li4 && <li>{li4}</li>}
-          </ul>
-
-          <Card.Text className=''>{text2}</Card.Text>
-        </Card.Body>
-      </Card>
-    );
-  })}
-</div>
- <div >
- <div className='web-tas-surec-title m-5'>
-      <h2 className='fs-1'>Hangi Teknolojileri Kullanıyoruz:</h2>
-    </div>
-    <div className='web-teknoloji-container d-flex justify-content-center'>
-    <Carousel className="carousel-custom">
-  {tekno.map((tech) => (
-    <Carousel.Item key={tech.id} className="carousel-item-custom">
-      <div className="d-flex justify-content-center ">
-        <img
-          className="d-block"
-          src={tech.image}
-          alt={tech.title}
-          style={{ height: '200px', objectFit: 'cover' }} 
-        />
+      <div className='web-tasarim-header w-100 d-flex justify-content-center'>
+        <img src={web} alt="" className='web-tasarim-img' />
       </div>
-      <div className="card border-5 p-4 text-center mt-2">
-        <h3 className='fs-1 fw-bold'>{tech.title}</h3>
-        <p className='fs-5'>{tech.description}</p>
+      <div className='web-tas-giris m-5 p-5 text-center'>
+        <h4 className='fs-2'>İnternet dünyasında markanızın güçlü bir izlenim bırakmasını sağlayacak bir web sitesi tasarlamak, uzmanlık ve stratejik bir süreç gerektirir.</h4>
       </div>
-    </Carousel.Item>
-  ))}
-</Carousel>
+      <div className='web-tas-surec-title m-5 pt-4'>
+        <h2 className='fs-1'>Web sitenizi tasarlarken izleyeceğimiz süreç:</h2>
+      </div>
+
+      {/* 3D Carousel Yapısı */}
+      <Carousel interval={5000} className='carousel-container d-flex align-items-center '>
+            {webtasarim.map((item, index) => (
+                <Carousel.Item key={item.id}>
+                    <div className="d-flex justify-content-around">
+                        <div className="card" style={{ width: '45%' }}>
+                            <div className="card-body">
+                            <h5 className="card-title fs-2">{item.id}.</h5>
+                                <h5 className="card-title fs-2">{item.title}</h5>
+                                <p className="card-text ">{item.text}</p>
+                                <ul>
+                                    <li>{item.li1}</li>
+                                    <li>{item.li2}</li>
+                                    {item.li3 && <li>{item.li3}</li>}
+                                    {item.li4 && <li>{item.li4}</li>}
+                                </ul>
+                                <p className="card-text">{item.text2}</p>
+                            </div>
+                        </div>
+                        {index + 1 < webtasarim.length && (
+                            <div className="card sonraki-card" style={{ width: '30%' }}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{webtasarim[index + 1].title}</h5>
+                                    <p className="card-text">{webtasarim[index + 1].text}</p>
+                                    <ul>
+                                        <li>{webtasarim[index + 1].li1}</li>
+                                        <li>{webtasarim[index + 1].li2}</li>
+                                        {webtasarim[index + 1].li3 && <li>{webtasarim[index + 1].li3}</li>}
+                                        {webtasarim[index + 1].li4 && <li>{webtasarim[index + 1].li4}</li>}
+                                    </ul>
+                                    <p className="card-text">{webtasarim[index + 1].text2}</p>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </Carousel.Item>
+            ))}
+        </Carousel>
+
+      {/* Hangi Teknolojileri Kullanıyoruz Bölümü */}
+      
+      <div className='web-teknoloji-container mt-5 p-5'>
+      <Carousel activeIndex={activeIndex} onSelect={handleSelect} className="carousel-custom tek-ana">
+        {tekno.map((tech) => (
+          <Carousel.Item key={tech.id} className="carousel-item-custom ">
+            <div className="carousel-image-container d-flex justify-content-center">
+              <img
+                className="carousel-image d-block"
+                src={tech.image}
+                alt={tech.title}
+                style={{ height: '200px', objectFit: 'cover' }} 
+              />
+            </div>
+            <div className="carousel-card border-5 p-4 text-center mt-2">
+              <h3 className='carousel-card-title fs-1 fw-bold'>{tech.title}</h3>
+              <p className='carousel-card-description fs-4'>{tech.description}</p>
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <div className="carousel-indicators-custom d-flex justify-content-center mt-5">
+        {tekno.map((tech, index) => (
+          <div
+            key={tech.id}
+            className={`indicator-image ${activeIndex === index ? 'active' : ''}`}
+            style={{
+              backgroundImage: `url(${tech.image})`,
+              width: '100px', 
+              height: '100px', 
+              borderRadius: '50%', 
+              margin: '0 5px',
+              backgroundSize: 'cover',
+              cursor: 'pointer',
+              border: activeIndex === index ? '2px solid #007bff' : 'none', 
+            }}
+            onClick={() => handleSelect(index)}
+          />
+        ))}
+      </div>
     </div>
- </div>
-    <div className='d-flex flex-wrap gap-5 justify-content-center' >
-    {teknoloji.map((tekno)=>{
-      const {id, image} = tekno
-      return(
-        <Card style={{ width: '5rem' }} key={id} className='border-0' >
-      <Card.Img variant="top" src={image} />
-   
-    </Card>
-
-      )
-    })}
-
-
-</div>
-
-   
-
-
-
-
-
 
     </div>
-  )
-}
+  );
+} 
 
-export default WebTasarım
+export default WebTasarim;
+
+
+
+
+
+
+
+
+
