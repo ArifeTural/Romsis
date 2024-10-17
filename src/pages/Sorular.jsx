958c25ca4d0c182313045917771ee4c689ac62c6
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-17 20:56:31 +0300

    gönder

--- src/pages/Sorular.jsx
+++ src/pages/Sorular.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { FaArrowCircleDown } from "react-icons/fa";
+import { FaArrowAltCircleUp } from "react-icons/fa";
+import { soruCevap } from '../helper/questions';
+import "../css/kurumsal.css"
+
 
 const Sorular = () => {
+
+  const [show, setShow] = useState(false);
+
+    
+  const toggleShow = (id) => {
+      setShow(show === id ? false : id);
+  };
+
   return (
-    <div>Sorular</div>
+    <div className='endis' >
+    <h2 className='text-center'>SIKÇA SORULAN SORULAR</h2>
+    {soruCevap.map(({ id, question, answer }) => (
+<Container className='soru-cont' key={id} >
+<div className="card w-100 mb-3">
+<div className="card-body">
+<p className="card-text baslik">
+             {id} - {question}
+</p>
+{show !== id ? (
+    <a href="#" className="icon-btn "
+     onClick={() => toggleShow(id)}
+     
+    >
+    <FaArrowCircleDown />
+    </a>
+) : (
+    <>
+        <div className="card w-100">
+            <div className="ikinci-kart">
+              
+                <p className="card-text">{answer}</p>
+                <button className="icon-btn " onClick={() => toggleShow(id)}>
+                <FaArrowAltCircleUp />
+                </button>
+            </div>
+        </div>
+    </>
+)}
+     </div>
+ </div>
+</Container>
+     ))}
+ </div>
   )
 }
 
 export default Sorular
+
