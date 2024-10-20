2e84ea9394d28b6074862c26ae5316b760afefc4
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-21 00:58:30 +0300

    vf

--- src/pages/ByOnBoard.jsx
+++ src/pages/ByOnBoard.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
 
 
 
 const ByOnBoard = () => {
   return (
     <div>
+
+<Carousel>
+      <Carousel.Item interval={1000}>
+        <img src="" alt="" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+    </Carousel>
 
     </div>
   )
