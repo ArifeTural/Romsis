58f5575a43549198db22ec89c294420fb7a27a99
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-20 16:55:27 +0300

    wms

--- src/pages/Wms.jsx
+++ src/pages/Wms.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
+import { Card, Container, Row, Col } from "react-bootstrap";
+
+import wmsgiris from "../assets/wms-giris.jpg"
+import wmshedef from "../assets/wms-hedefler.jpg"
+import tablo from "../assets/wms-tablo.png"
 
 const Wms = () => {
+
+  const features = [
+    "Kullanıcı dostu bir arayüz tasarlanması",
+    "Kullanıcılara uygun görüntülenmek üzere web sitesinin oluşturulması",
+    "Android ve IOS tabanlı mobil uygulamanın oluşturulması",
+    "Müşteri istek ve önerilerinin test edilebileceği bir ön dizinleme sisteminin geliştirilmesi",
+    "Müşteri geri bildirim ve ek istek tekliflerinin onay süreçlerinin takibi ve sonucun bildirimi",
+    "Kişiselleştirilebilir bir yazılım olması",
+    "Faturalandırma ve ödeme sistemlerinin geliştirilmesi ve takibi",
+  ];
   return (
-    <div>Wms</div>
+    <div>
+
+      <div>
+        <h1 className='p-4 text-center'>WMS Depo Yönetim Sistemleri: Verimli ve Akıllı Depo Çözümleri</h1>
+      </div>
+      <img src={wmsgiris} alt="wmsgiris" className='w-100 p-3' />
+
+    <div>
+      <p className='text-center'>"WMS Depo Yönetim Sistemleri ile uluslararası depolama alanınızı bir şehir mimarı gibi planlayarak adresleyin, tedarik zincirinizi ve tedarikçilerinizi anında kontrol edip değerlendirin. Doğru zamanda doğru ürünü müşterilerinize gönderirken, üretimden gelen girişlerinizi ve sevklerinizi zamanında ve kontrollü bir şekilde yönetin. Depolar arası sevkiyatlarınızı güvenli ve verimli hale getirirken, kullanıcı dostu ve kolay arayüzümüzle işlemlerinizi sorunsuzca gerçekleştirin. Ek ihtiyaçlarınıza göre özelleştirilmiş çözümler sunarak, iş süreçlerinizi optimize ediyoruz."</p>
+    </div>
+
+    <Container className="text-center">
+    <h2>Nasıl Çalışıyoruz: Süreçlerimiz ve Yaklaşımımız</h2>
+      <Row className='justify-content-center align-items-center'>
+        <Col sm={12} md={4} className="mb-4">
+          <img src={wmshedef} alt="hedefler" className="img-fluid rounded-pill" />
+        </Col>
+        <Col sm={12} md={8}>
+          <Row>
+            {features.map((feature, index) => (
+              <Col key={index} sm={12}  className="mb-4 ">
+                <Card className='border-0'>
+              
+                    <Card.Text>
+                      {index + 1}. {feature}
+                    </Card.Text>
+    
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+
+<Container>
+  <h2>ROMSİS WMS, MRP DEPO ÇÖZÜM MODÜLLERİ</h2>
+  <div>
+    <img src={tablo} alt="tablo"  />
+  </div>
+
+</Container>
+
+
+
+
+
+
+    </div>
   )
 }
 
