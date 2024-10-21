3678ff04ce5bd8eddee27fc4659e021d8a336049
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-21 13:26:22 +0300

    düzenleme

--- src/pages/ByOnBoard.jsx
+++ src/pages/ByOnBoard.jsx
@@ -6,74 +6,106 @@ import '../css/byOnBoard.css'; // CSS dosyanızı ekleyin
 import { Col, Container, Row } from 'react-bootstrap';
 import { board } from '../helper/referans';
 
+
+const CarouselItem = React.memo(({ buy }) => {
+  const { id, image, h1 } = buy;
+  return (
+    <Carousel.Item interval={10000} key={id}>
+      <img
+        src={image}
+        alt={`Uçak İçi Hizmet: ${h1}`}
+        style={{
+          width: '90%',
+          height: '80vh',
+          display: 'block',
+          margin: 'auto',
+          borderRadius: '3rem',
+        }}
+        loading="lazy" // Lazy loading
+      />
+      <Carousel.Caption className="buyon-caption">
+        <h3 className="byon-h3">{h1}</h3>
+      </Carousel.Caption>
+    </Carousel.Item>
+  );
+});
+
 const ByOnBoard = () => {
   return (
-    <div> 
+    <div>
       <Helmet>
         <title>Buy on Board Hizmetleri | Romsis</title>
-        <meta name="description" content="Romsis olarak modern havayolu deneyimini zenginleştiriyoruz. Buy on Board hizmetleri ile yolculara çeşitli yiyecek ve içecek seçenekleri sunuyoruz." />
+        <meta
+          name="description"
+          content="Romsis olarak modern havayolu deneyimini zenginleştiriyoruz. Buy on Board hizmetleri ile yolculara çeşitli yiyecek ve içecek seçenekleri sunuyoruz."
+        />
       </Helmet>
 
+      {/* Carousel */}
       <div className="carousel-buyon">
         <Carousel>
-          {buyOnCarousel.map((buy) => {
-            const { id, image, h1 } = buy;
-            return (
-              <Carousel.Item interval={10000} key={id}>
-                <img src={image} alt={`Uçak İçi Hizmet: ${h1}`} style={{ width: "90%", height: "80vh", display: 'block', margin: 'auto', borderRadius: '3rem' }} />
-                <Carousel.Caption className="buyon-caption">
-                  <h3 className='byon-h3'>{h1}</h3> 
-                </Carousel.Caption>
-              </Carousel.Item>
-            );
-          })}
+          {buyOnCarousel && buyOnCarousel.length > 0 && buyOnCarousel.map((buy) => (
+            <CarouselItem key={buy.id} buy={buy} />
+          ))}
         </Carousel>
       </div>
 
       <Container>
-        <p className='buyon-par'>
+        <p className="buyon-par">
           Romsis olarak, "Buy on Board" (BOB) hizmetleri ile modern havayolu deneyimini zenginleştiriyoruz. Uçuş sırasında yolcularımıza çeşitli yiyecek ve içecek seçenekleri sunarak konforlarını artırıyor, aynı zamanda havayolu şirketlerine ek gelir imkanı sağlıyoruz. Yolcular, tercih ettikleri ürünleri kolayca seçerken, biz de etkin bir şekilde uçak içi hizmetlerini yönetiyor ve satış işlemlerini sorunsuz bir şekilde gerçekleştiriyoruz. Romsis, "Buy on Board" sistemi ile yolcuların seyahatlerini daha keyifli hale getirirken, havayolu şirketlerinin operasyonel verimliliğini artırmayı hedefliyoruz.
         </p>
       </Container>
 
-      <Container className='p-3'>
-        {bob.map((bobb, index) => {
+      <Container className="p-3">
+        {bob && bob.length > 0 && bob.map((bobb, index) => {
           const { id, image, h3, description } = bobb;
 
-          // İkinci kart için özel bir stil belirleyelim
           const isSecondCard = index === 1;
 
           return (
-            <Row className="g-3 mb-4 justify-content-between align-items-center" key={id}>
+            <Row
+              className="g-3 mb-4 justify-content-between align-items-center"
+              key={id}
+            >
               {isSecondCard ? (
                 <>
-                  <Col className="col-md-8 ">
+                  <Col className="col-md-8">
                     <div className="card-body">
-                      <h5 className="card-title fw-bold fs-2 text-center bob-title">{h3}</h5>
-                      <p className="card-text fs-5 text-center bob-par">{description}</p>
+                      <h5 className="card-title fw-bold fs-2 text-center bob-title">
+                        {h3}
+                      </h5>
+                      <p className="card-text fs-5 text-center bob-par">
+                        {description}
+                      </p>
                     </div>
                   </Col>
                   <Col className="col-md-4">
-                    <img 
-                      src={image} 
-                      className="img-fluid rounded-5 bob-image" 
-                      alt={`Hizmet: ${h3}`} 
+                    <img
+                      src={image}
+                      className="img-fluid rounded-5 bob-image"
+                      alt={`Hizmet: ${h3}`}
+                      loading="lazy" // Lazy loading
                     />
                   </Col>
                 </>
               ) : (
                 <>
                   <Col className="col-md-4">
-                    <img 
-                      src={image} 
-                      className="img-fluid rounded-5 bob-image" 
-                      alt={`Hizmet: ${h3}`} 
+                    <img
+                      src={image}
+                      className="img-fluid rounded-5 bob-image"
+                      alt={`Hizmet: ${h3}`}
+                      loading="lazy" // Lazy loading
                     />
                   </Col>
                   <Col className="col-md-8">
                     <div className="card-body">
-                      <h5 className="card-title fw-bold fs-2 text-center bob-title">{h3}</h5>
-                      <p className="card-text fs-5 text-center bob-par">{description}</p>
+                      <h5 className="card-title fw-bold fs-2 text-center bob-title">
+                        {h3}
+                      </h5>
+                      <p className="card-text fs-5 text-center bob-par">
+                        {description}
+                      </p>
                     </div>
                   </Col>
                 </>
@@ -83,26 +115,38 @@ const ByOnBoard = () => {
         })}
       </Container>
 
-      <div className='text-center'>
+      <div className="text-center">
         <h2>
-          <a href="#" className='h-refe font-monospace fw-bolder fs-1 text-secondary'>İş Ortaklarımız</a>
+          <a
+            href="#"
+            className="h-refe font-monospace fw-bolder fs-1 text-secondary"
+          >
+            İş Ortaklarımız
+          </a>
         </h2>
       </div>
-      <div className='referans'>
-        {board.map((arife) => {
+
+      <div className="referans">
+        {board && board.length > 0 && board.map((arife) => {
           const { id, image } = arife;
           return (
-            <div className="referans-card" style={{ width: "8rem" }} key={id}>
-              <img src={image} className="card-img-top" alt="İş ortağı logosu" />
+            <div className="referans-card" style={{ width: '8rem' }} key={id}>
+              <img
+                src={image}
+                className="card-img-top"
+                alt="İş ortağı logosu"
+                loading="lazy" // Lazy loading
+              />
             </div>
           );
         })}
       </div>
     </div>
   );
-}
+};
 
 export default ByOnBoard;
+
 
 
 
--- src/pages/Inflight.jsx
+++ src/pages/Inflight.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Card, Carousel, Modal, Button } from 'react-bootstrap';
-import ucak from "../assets/romsisucak.png"
+import { Helmet } from 'react-helmet';
+import ucak from "../assets/romsisucak.png";
 import { services } from '../helper/yazilim';
-import infli from "../assets/inflightgiriss.jpg"
+import infli from "../assets/inflightgiriss.jpg";
 import { board } from '../helper/referans';
+import menu from "../assets/menu.png";
 
 const Inflight = () => {
   const [showModal, setShowModal] = useState(false);
   const [selectedService, setSelectedService] = useState(null);
-
-
 
   const aboutText = "Uçuşlarımızda siz değerli yolcularımıza en iyi hizmeti sunmayı amaçlıyoruz. Yenilikçi yaklaşımlarımızla seyahatinizi daha keyifli hale getiriyoruz.";
 
@@ -26,9 +26,22 @@ const Inflight = () => {
 
   return (
     <div>
+      {/* Helmet for SEO */}
+      <Helmet>
+        <title>Uçuş Hizmetleri | Romsis - En İyi In-Flight Deneyimi</title>
+        <meta name="description" content="Romsis, uçuş sırasında konforunuzu ön planda tutarak en iyi in-flight hizmetleri sunar. Yiyecek içecek servisi, eğlence seçenekleri, Wi-Fi hizmeti ve daha fazlası ile uçuşlarınız keyifli geçsin." />
+        <meta name="keywords" content="uçuş hizmetleri, in-flight deneyimi, Wi-Fi uçuş, eğlence uçuş, Romsis, yiyecek içecek uçuş" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Romsis - Uçuş Hizmetleri" />
+        <meta property="og:description" content="Romsis uçuşlarında en iyi hizmetleri sunarak seyahatinizi daha konforlu hale getiriyor." />
+        <meta property="og:image" content={infli} />
+        <meta property="og:type" content="website" />
+      </Helmet>
+
       <div>
         <img src={infli} alt="inflightgiris" style={{width: "100%"}} />
       </div>
+
       <Container className="p-2">
         <h1 className='inflight-h1'>Uçuş Hizmetleri | En Iyi In-Flight Deneyimi</h1>
         <p className='inflight-par p-5'>Uçuş sırasında konforunuzu ön planda tutarak sunduğumuz kaliteli hizmetlerle seyahatinizi daha keyifli hale getiriyoruz. Yiyecek ve içecek servisi, eğlence seçenekleri ve Wi-Fi erişimi gibi birçok imkan ile siz değerli yolcularımızın memnuniyetini sağlamak için buradayız.</p>
@@ -42,10 +55,10 @@ const Inflight = () => {
       {/* Hizmetler Bölümü */}
       <h2 className='text-center hizmet-h2'>Hizmetlerimiz</h2>   
       <Container className="">
-        <Row>
+        <Row className=' mx-auto '>
           {services.map((service, index) => (
             <Col key={index} md={4} >
-              <Card className='hizmet-kart shadow-lg' style={{ width: '23rem', height: '32rem' }}>
+              <Card className='hizmet-kart shadow-lg' style={{ width: '20rem', height: '32rem' }}>
                 <Card.Img variant="top" src={service.imageUrl} style={{height:"300px"}} />
                 <Card.Body>
                   <Card.Title className='hizmet-kart-baslik'>{service.title}</Card.Title>
@@ -58,28 +71,23 @@ const Inflight = () => {
         </Row>
       </Container>
 
-    
       <Container>
-      <Row className="align-items-center">
-        {/* Resim Kolonu */}
-        <Col xs={12} sm={6} className="text-center mb-3 mb-sm-0">
-          <img src={ucak} alt="romsisucak" className="img-fluid" />
-        </Col>
-
-        {/* SSS Kolonu */}
-        <Col xs={12} sm={6} className="d-flex justify-content-center">
-          <ul className="list-unstyled mt-3">
-            <li className='fs-4'><strong>Uçuş sırasında Wi-Fi mevcut mu?</strong> <br/> Evet, uçuşlarımızda Wi-Fi hizmeti bulunmaktadır.</li>
-            <li className='fs-4'><strong>Yiyecek ve içecek servisi nasıl yapılıyor?</strong>
-            <br/> Uçuş sırasında menümüzden seçtiğiniz yiyecek ve içecekleri sunuyoruz.</li>
-            <li className='fs-4'><strong>Uçuş süresince eğlence seçenekleri var mı?</strong>
-            <br/> Evet, uçuş süresince birçok film, dizi ve müzik seçeneği sunuyoruz.</li>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
-
-
+        <h2 className='hizmet-h2'>Sıkça Sorulan Sorular</h2>
+        <Row className="align-items-center">
+          <Col xs={12} sm={6} className="text-center mb-3 mb-sm-0">
+            <img src={ucak} alt="romsisucak" style={{height: "300px"}} />
+          </Col>
+          <Col xs={12} sm={6} className="d-flex justify-content-center">
+            <ul className="list-unstyled mt-3">
+              <li className='fs-4 text-secondary'><strong>Uçuş sırasında Wi-Fi mevcut mu?</strong> <br/> Evet, uçuşlarımızda Wi-Fi hizmeti bulunmaktadır.</li>
+              <li className='fs-4 text-secondary'><strong>Yiyecek ve içecek servisi nasıl yapılıyor?</strong>
+              <br/> Uçuş sırasında menümüzden seçtiğiniz yiyecek ve içecekleri sunuyoruz.</li>
+              <li className='fs-4 text-secondary'><strong>Uçuş süresince eğlence seçenekleri var mı?</strong>
+              <br/> Evet, uçuş süresince birçok film, dizi ve müzik seçeneği sunuyoruz.</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
 
       {/* Modal Bölümü */}
       <Modal show={showModal} onHide={handleClose}>
@@ -87,7 +95,7 @@ const Inflight = () => {
           <Modal.Title className='hizmet-kart-baslik'>{selectedService ? selectedService.title : ''}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p  className='hizmet-kart-text'><strong>Açıklama:</strong> {selectedService ? selectedService.details : ''}</p>
+          <p className='hizmet-kart-text'><strong>Açıklama:</strong> {selectedService ? selectedService.details : ''}</p>
           <p className='hizmet-kart-text'><strong>Avantajlar:</strong></p>
           <ul className='hizmet-kart-text'>
             {selectedService && selectedService.advantages.map((advantage, index) => (
@@ -96,12 +104,18 @@ const Inflight = () => {
           </ul>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}  className='hizmet-kart-buton' >
+          <Button variant="secondary" onClick={handleClose} className='hizmet-kart-buton'>
             Kapat
           </Button>
         </Modal.Footer>
       </Modal>
 
+      <div className='mb-5 pb-5'>
+        <h2 className='text-center hizmet-kart-baslik fs-2'>Örnek Menümüz</h2>
+        <img src={menu} alt="menu" className='w-100' />
+      </div>
+
+      {/* REFERANSLARIMIZ */}
       <div className='text-center'>
         <h2>
           <a href="#" className='h-refe font-monospace fw-bolder fs-1 text-secondary'>İş Ortaklarımız</a>
@@ -117,8 +131,6 @@ const Inflight = () => {
           );
         })}
       </div>
-
-
     </div>
   );
 };
