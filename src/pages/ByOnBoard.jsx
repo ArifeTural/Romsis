e67f692e781dd887ee1995c53adc459fce4315b3
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-28 03:05:39 +0300

    yalın üretim

--- src/pages/ByOnBoard.jsx
+++ src/pages/ByOnBoard.jsx
@@ -21,7 +21,7 @@ const ByOnBoard = () => {
             const { id, image, h1 } = buy;
             return (
               <Carousel.Item interval={10000} key={id}>
-                <img src={image} alt={`Uçak İçi Hizmet: ${h1}`} style={{ width: "90%", height: "80vh", display: 'block', margin: 'auto', borderRadius: '3rem' }} />
+                <img src={image} alt={`Uçak İçi Hizmet: ${h1}`} style={{ width: "90%", height: "80vh", display: 'block', margin: 'auto', borderRadius: '1rem' }} />
                 <Carousel.Caption className="buyon-caption">
                   <h3 className='byon-h3'>{h1}</h3> 
                 </Carousel.Caption>
@@ -36,25 +36,26 @@ const ByOnBoard = () => {
           Romsis olarak, "Buy on Board" (BOB) hizmetleri ile modern havayolu deneyimini zenginleştiriyoruz. Uçuş sırasında yolcularımıza çeşitli yiyecek ve içecek seçenekleri sunarak konforlarını artırıyor, aynı zamanda havayolu şirketlerine ek gelir imkanı sağlıyoruz. Yolcular, tercih ettikleri ürünleri kolayca seçerken, biz de etkin bir şekilde uçak içi hizmetlerini yönetiyor ve satış işlemlerini sorunsuz bir şekilde gerçekleştiriyoruz. Romsis, "Buy on Board" sistemi ile yolcuların seyahatlerini daha keyifli hale getirirken, havayolu şirketlerinin operasyonel verimliliğini artırmayı hedefliyoruz.
         </p>
       </Container>
+      
 
       <Container className='p-3'>
         {bob.map((bobb, index) => {
           const { id, image, h3, description } = bobb;
 
-          // İkinci kart için özel bir stil belirleyelim
+          // İkinci kart için özel bir stil 
           const isSecondCard = index === 1;
 
           return (
-            <Row className="g-3 mb-4 justify-content-between align-items-center" key={id}>
+            <Row className="g-3  mb-4 justify-content-between align-items-center" key={id}>
               {isSecondCard ? (
                 <>
-                  <Col className="col-md-8 ">
+                  <Col className=" col-12 col-md-8 ">
                     <div className="card-body">
                       <h5 className="card-title fw-bold fs-2 text-center bob-title">{h3}</h5>
                       <p className="card-text fs-5 text-center bob-par">{description}</p>
                     </div>
                   </Col>
-                  <Col className="col-md-4">
+                  <Col className="col-12 col-md-4 ">
                     <img 
                       src={image} 
                       className="img-fluid rounded-5 bob-image" 
@@ -71,7 +72,7 @@ const ByOnBoard = () => {
                       alt={`Hizmet: ${h3}`} 
                     />
                   </Col>
-                  <Col className="col-md-8">
+                  <Col className="col-12 col-md-8">
                     <div className="card-body">
                       <h5 className="card-title fw-bold fs-2 text-center bob-title">{h3}</h5>
                       <p className="card-text fs-5 text-center bob-par">{description}</p>
--- src/pages/WebTasarim.jsx
+++ src/pages/WebTasarim.jsx
@@ -1,186 +1,179 @@
-import React, {useState} from 'react';
-import { tekno} from '../helper/yazilim';
-import girisimg from "../assets/web-tasarim-giris.jpg"
-import soru from "../assets/question-mark.png"
-import { steps } from '../helper/yazilim';
-import { Carousel, Container, Row, Col } from 'react-bootstrap';
-import { Helmet } from 'react-helmet';
-import "../css/web.css"
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet'; 
+import { Container, Row, Col, Card, Button, Collapse } from 'react-bootstrap';
 
-const WebTasarim = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+function WebTasarim() {
+  const [open, setOpen] = useState({}); 
 
-  const handleSelect = (selectedIndex) => {
-    setActiveIndex(selectedIndex);
+  const toggleExample = (title) => {
+    setOpen((prevOpen) => ({
+      ...prevOpen,
+      [title]: !prevOpen[title],
+    }));
   };
 
-
-  const hizmetler = [
-    'Kullanıcı Deneyimi (UX) ve Arayüz Tasarımı (UI)',
-    'Mobil Uyumluluk ve Responsive Tasarım',
-    'Özel Tasarım Çözümleri',
-    'E-Ticaret Tasarımı',
-    'SEO Uyumlu Web Tasarım',
-    'Hız ve Performans Optimizasyonu',
-    'Entegrasyon ve Yönetim Sistemleri',
-    'İçerik Yönetim Sistemleri (CMS)',
-    'Web Uygulama Geliştirme'
-  ];
-
   return (
-<div>
-
-<Helmet>
-        <title>Web Tasarım Hizmetleri</title>
-        <meta name="description" content="Modern, kullanıcı odaklı ve SEO uyumlu web tasarım çözümlerimizle markanızı dijitalde öne çıkarın. Hız ve performans optimizasyonu ile web sitenizi güçlendirin." />
-</Helmet>
-<div>
-  <img src={girisimg} alt="" className='w-100 ' />
-</div>
- <div>
-  <h1 className='text-center fs-1  p-5 tasarim-baslik'>Dijital Başarınız için Modern ve Kullanıcı Odaklı Web Tasarım Hizmetleri</h1>
-  <p className='fs-2 text-center tasarim-par'>Markanızın dijitaldeki yüzünü güçlü kılmak için modern, kullanıcı odaklı ve mobil uyumlu web tasarımlarımızla fark yaratıyoruz. SEO uyumlu çözümlerimizle, hedef kitlenize kolayca ulaşmanızı sağlıyoruz.</p>
- </div>
-
-
- <div className="container my-4  ">
- <h2 className='fs-1 fw-bolder text-center tasarim-baslikk '> Nasıl Çalışıyoruz ?</h2>
-      <div className="row">
-        {steps.map((step, index) => (
-          <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-2 mb-4 d-flex justify-content-center">
-            <div className="card border-0">
-              <img 
-               className="nasil-img " 
-                src={step.img} 
-                alt={step.alt} 
-                loading="lazy" 
-              />
-              <div className="card-body">
-                <h5 className="card-title fs-4 fw-bold text-secondary">{step.title}</h5>
-                <ul className="list-unstyled ">
-                  {step.content.map((item, i) => (
-                    <li key={i} className="text-muted ">
-                      * {item}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-
-
- <div className='mt-5 d-flex flex-wrap justify-content-center align-items-center gap-5'>
-<div>
-<h2 className='fs-1 fw-bolder mb-4 tasarim-baslikk' >Neler Yapıyoruz?</h2>
-  <img src={soru} alt="soruisareti" width={200}/>
-</div>
-   
-      <ul>
-        {hizmetler.map((hizmet, index) => (
-          <li key={index} className='fs-4 ml-5 neler-ul' >{hizmet}</li>
-        ))}
-      </ul>
-    </div>
-
-
-
-    <Container className="my-5">
-  <h2 className="text-center tasarim-baslikk fw-bold fs-1 mb-4">Proje Özellikleri</h2>
-  <Row className='justify-content-center gap-2'>
-    <Col md={3} className="text-center p-3 border  border-3 border-black ">
-   
-      <h5 className="font-weight-bold">Mobil Uyumlu Tasarım</h5>
-      <p>Responsive tasarım prensipleriyle web sitenizin tüm cihazlarda mükemmel bir kullanıcı deneyimi sunmasını sağlıyoruz. Mobil uyumlu web siteleri ile daha fazla ziyaretçi çekebilir ve SEO performansınızı artırabilirsiniz.</p>
-    </Col>
-    <Col md={3} className="text-center p-3 border border-3  border-warning">
-    
-      <h5 className="font-weight-bold ">Web Hızı Optimizasyonu</h5>
-      <p>Özgün ve optimize edilmiş kodlama yöntemlerimizle web sitenizin yükleme hızını en üst seviyeye çıkarıyoruz. Hızlı yükleme süreleri, kullanıcı memnuniyetini artırırken arama motoru sıralamalarınızı olumlu yönde etkiler.</p>
-    </Col>
-    <Col md={3} className="text-center p-3 border border-3 border-info">
- 
-      <h5 className="font-weight-bold">Özgün ve Yenilikçi Tasarım</h5>
-      <p>Web sitenizi benzersiz bir tasarım anlayışı ile oluşturuyor, kullanıcıların ilgisini çekecek özgün içeriklerle zenginleştiriyoruz. Özgün tasarımlar, markanızın imajını güçlendirir ve dönüşüm oranlarını artırır.</p>
-    </Col>
-    <Col md={3} className="text-center p-3 border border-3 border-danger">
-   
-      <h5 className="font-weight-bold">Web Standartlarına Uygunluk</h5>
-      <p>Web tasarım ve geliştirme süreçlerimizi uluslararası web standartlarına uygun bir şekilde gerçekleştiriyoruz. Bu, sitenizin daha güvenilir olmasını ve SEO uyumlu hale gelmesini sağlar.</p>
-    </Col>
-  
-    <Col md={3} className="text-center p-3 border border-3 border-success">
-  
-      <h5 className="font-weight-bold">Temiz ve Okunabilir Kodlama</h5>
-      <p>Web sitenizde karmaşık kodlamalardan kaçınarak, okunabilir ve sürdürülebilir bir kod yapısı oluşturuyoruz. Temiz kodlama, web sitenizin performansını artırır ve SEO çalışmalarını kolaylaştırır.</p>
-    </Col>
-    <Col md={3} className="text-center p-3 border border-3 border-secondary">
-  
-      <h5 className="font-weight-bold">SEO Odaklı Yaklaşım</h5>
-      <p>SEO stratejilerimizi projenizin başlangıcından itibaren entegre ediyoruz. SEO odaklı bir yaklaşım, arama motorlarıyla görünürlüğünüzü artırır ve organik trafiğinizi önemli ölçüde yükseltir.</p>
-    </Col>
-    </Row>
-
-</Container>
-
-<div>
-  <h2 className='tasarim-baslikk m-5 pt-4 fs-1 text-center'>Hangi Teknolojileri Kullanıyoruz:</h2>
-</div>
-<div className='web-teknoloji-container d-flex flex-column align-items-center justify-content-center flex-wrap gap-3 w-75 mx-auto'>
-  <Carousel activeIndex={activeIndex} onSelect={handleSelect} className="carousel-custom tek-ana w-100" indicators={false}>
-    {tekno.map((tech) => (
-      <Carousel.Item key={tech.id} className="carousel-item-custom">
-        <div className="carousel-image-container d-flex justify-content-center">
-          <img
-            className="carousel-image d-block mb-3"
-            src={tech.image}
-            alt={tech.title}
-            style={{ height: '200px', objectFit: 'cover', maxWidth: '100%' }}
-          />
-        </div>
-        <div className="carousel-card border-5 text-center mt-1"> 
-          <h3 className='carousel-card-title fs-4 fw-bold'>{tech.title}</h3>
-          <p className='carousel-card-description'>{tech.description}</p>
-        </div>
-      </Carousel.Item>
-    ))}
-  </Carousel>
-
-
-  <div className="carousel-indicators-custom d-flex flex-wrap justify-content-center mt-3">
-    {tekno.map((tech, index) => (
-      <div
-        key={tech.id}
-        className={`indicator-image ${activeIndex === index ? 'active' : ''}`}
-        style={{
-          backgroundImage: `url(${tech.image})`,
-          width: '50px',
-          height: '50px',
-          borderRadius: '50%',
-          margin: '0 5px',
-          backgroundSize: 'cover',
-          cursor: 'pointer',
-          border: activeIndex === index ? '2px solid #007bff' : 'none',
-        }}
-        onClick={() => handleSelect(index)}
-      />
-    ))}
-  </div>
-</div>
-
-
- 
-
-
-
-
-</div>
+    <>
+      <Helmet>
+        <title>Yalın Üretim - Verimlilik ve Süreç İyileştirme</title>
+        <meta name="description" content="Yalın üretim süreçleri ile üretim verimliliğinizi artırın. Eksik süreçleri tamamlayın, gereksiz süreçleri kaldırın." />
+      </Helmet>
+      <Container className="mt-5 cont-yalin border border-danger">
+        <h1 className="text-center mb-4">Yalın Üretim</h1>
+        <p className="text-center mb-5">
+          Yalın üretim, israfı minimize ederek, değer yaratmaya odaklanan bir üretim felsefesidir. Hedefimiz, üretim süreçlerini optimize ederek işletmenizin verimliliğini artırmaktır.
+        </p>
+        <Row >
+          <Col sm={12} md={6}>
+            <Card className="mb-3 shadow-sm">
+              <Card.Body >
+                <Card.Title>Uçtan Uca Üretim Proses Analizi</Card.Title>
+                <Card.Text>
+                  Üretim yapan müşterilerimizin tüm üretim süreçlerini baştan sona analiz ediyoruz.
+                </Card.Text>
+                <Button 
+                  variant="primary" 
+                  onClick={() => toggleExample("Uçtan Uca Üretim Proses Analizi")}
+                  aria-controls="example-collapse-Uçtan-Uca-Üretim-Proses-Analizi"
+                  aria-expanded={open["Uçtan Uca Üretim Proses Analizi"]}
+                >
+                  Örnek
+                </Button>
+                <Collapse in={open["Uçtan Uca Üretim Proses Analizi"]}>
+                  <div id="example-collapse-Uçtan-Uca-Üretim-Proses-Analizi">
+                    <strong>Örnek:</strong> Bir otomotiv üreticisi için, montaj hattındaki her adımı gözden geçirerek zaman kayıplarını ve darboğazları belirliyoruz.
+                  </div>
+                </Collapse>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={12} md={6}>
+            <Card className="mb-3 shadow-sm  yalin-card">
+              <Card.Body>
+                <Card.Title>Eksik Süreçlerin Tamamlanması</Card.Title>
+                <Card.Text>
+                  Eksik süreçleri tamamlayarak üretimdeki aksaklıkları gideriyoruz.
+                </Card.Text>
+                <Button 
+                  variant="primary" 
+                  onClick={() => toggleExample("Eksik Süreçlerin Tamamlanması")}
+                  aria-controls="example-collapse-Eksik-Süreçlerin-Tamamlanması"
+                  aria-expanded={open["Eksik Süreçlerin Tamamlanması"]}
+                >
+                  Örnek
+                </Button>
+                <Collapse in={open["Eksik Süreçlerin Tamamlanması"]}>
+                  <div id="example-collapse-Eksik-Süreçlerin-Tamamlanması">
+                    <strong>Örnek:</strong> Geri bildirimlerden yola çıkarak, ekipman kullanımında yaşanan sorunları çözüyoruz ve gerekli eğitimleri sağlıyoruz.
+                  </div>
+                </Collapse>
+              </Card.Body>
+            </Card>
+          </Col>
+      
+          <Col sm={12} md={6}>
+            <Card className="mb-3 shadow-sm  yalin-card">
+              <Card.Body>
+                <Card.Title>Gereksiz Süreçlerin Kaldırılması</Card.Title>
+                <Card.Text>
+                  Gereksiz süreçleri kaldırarak maliyetleri azaltıyor ve daha verimli bir üretim sağlıyoruz.
+                </Card.Text>
+                <Button 
+                  variant="primary" 
+                  onClick={() => toggleExample("Gereksiz Süreçlerin Kaldırılması")}
+                  aria-controls="example-collapse-Gereksiz-Süreçlerin-Kaldırılması"
+                  aria-expanded={open["Gereksiz Süreçlerin Kaldırılması"]}
+                >
+                  Örnek
+                </Button>
+                <Collapse in={open["Gereksiz Süreçlerin Kaldırılması"]}>
+                  <div id="example-collapse-Gereksiz-Süreçlerin-Kaldırılması">
+                    <strong>Örnek:</strong> Depolama süreçlerini optimize ederek, envanterdeki ürünlerin daha hızlı hareket etmesini sağlıyoruz.
+                  </div>
+                </Collapse>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={12} md={6}>
+            <Card className="mb-3 shadow-sm  yalin-card">
+              <Card.Body>
+                <Card.Title>Verimlilik Artışı Sağlama</Card.Title>
+                <Card.Text>
+                  Verimliliği artırmak için sürekli iyileştirme çalışmalarını yürütüyoruz.
+                </Card.Text>
+                <Button 
+                  variant="primary" 
+                  onClick={() => toggleExample("Verimlilik Artışı Sağlama")}
+                  aria-controls="example-collapse-Verimlilik-Artışı-Sağlama"
+                  aria-expanded={open["Verimlilik Artışı Sağlama"]}
+                >
+                  Örnek
+                </Button>
+                <Collapse in={open["Verimlilik Artışı Sağlama"]}>
+                  <div id="example-collapse-Verimlilik-Artışı-Sağlama">
+                    <strong>Örnek:</strong> Yapay zeka destekli süreç izleme sistemleri kurarak, anlık verimlilik oranlarını takip ediyoruz.
+                  </div>
+                </Collapse>
+              </Card.Body>
+            </Card>
+          </Col>
+      
+          <Col sm={12} md={6}>
+            <Card className="mb-3 shadow-sm  yalin-card">
+              <Card.Body>
+                <Card.Title>İzlenebilir ve Yönetilebilir İş Akışları</Card.Title>
+                <Card.Text>
+                  İş akışlarını detaylandırarak izlenebilir ve yönetilebilir üretim süreçleri sunuyoruz.Bu, yöneticilerin süreçleri daha iyi yönetmesine ve performansı artırmasına olanak tanır.
+                </Card.Text>
+                <Button 
+                  variant="primary" 
+                  onClick={() => toggleExample("İzlenebilir ve Yönetilebilir İş Akışları")}
+                  aria-controls="example-collapse-İzlenebilir-ve-Yönetilebilir-İş-Akışları"
+                  aria-expanded={open["İzlenebilir ve Yönetilebilir İş Akışları"]}
+                >
+                  Örnek
+                </Button>
+                <Collapse in={open["İzlenebilir ve Yönetilebilir İş Akışları"]}>
+                  <div id="example-collapse-İzlenebilir-ve-Yönetilebilir-İş-Akışları">
+                    <strong>Örnek:</strong> Üretim hatlarımızda gerçek zamanlı izleme sistemleri kurarak, her aşamanın verimliliğini anlık olarak raporluyoruz.
+                  </div>
+                </Collapse>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={12} md={6}>
+            <Card className="mb-3 shadow-sm  yalin-card">
+              <Card.Body>
+                <Card.Title>Enerji Verimliliği</Card.Title>
+                <Card.Text>
+                  Enerji tüketimini azaltmak için sürdürülebilir yöntemler ve teknolojiler uyguluyoruz.
+                </Card.Text>
+                <Button 
+                  variant="primary" 
+                  onClick={() => toggleExample("Enerji Verimliliği")}
+                  aria-controls="example-collapse-Enerji-Verimliliği"
+                  aria-expanded={open["Enerji Verimliliği"]}
+                >
+                  Örnek
+                </Button>
+                <Collapse in={open["Enerji Verimliliği"]}>
+                  <div id="example-collapse-Enerji-Verimliliği">
+                    <strong>Örnek:</strong> Enerji tasarrufu sağlayan ekipmanlar kullanarak, üretim maliyetlerimizi düşürüyoruz.
+                  </div>
+                </Collapse>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
 export default WebTasarim;
+
+
+
 
 
 
