b4df80beef74094dcab2a4ef73557afb8ad20a58
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-10-30 02:08:51 +0300

    yalin üretim mrp

--- src/pages/Yalin.jsx
+++ src/pages/Yalin.jsx
@@ -4,6 +4,8 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import "../css/web.css"
 import yalin from "../assets/istockphoto-85406187-1024x1024.jpg"
 import { yalinuretim } from '../helper/data';
+import mrp from "../assets/istockphoto-1477516505-1024x1024.jpg"
+import mrp2 from "../assets/liste.jpg"
 
 function Yalin() {
 
@@ -35,13 +37,13 @@ function Yalin() {
         <Row className='yalin-cont'>
           {yalinuretim.map((item, index) => (
             <Col sm={12} md={6} key={index}>
-              <Card className="mb-3 shadow-lg border-0 text-center">
+              <Card className="mb-3 shadow-lg border-0 text-center yalin-kart">
                 <Card.Body>
-                  <Card.Title className='fs-3 fw-bold'>{item.title}</Card.Title>
+                  <Card.Title className='fs-3 fw-bold text-secondary'>{item.title}</Card.Title>
                   <Card.Text>
                     {item.text}
                   </Card.Text>
-                  <Button variant="primary" onClick={() => toggleExample(index)}>
+                  <Button className='yalin-but' variant="primary" onClick={() => toggleExample(index)}>
                     {showExamples[index] ? "Örneği Kapat" : "Örneği Aç"}
                   </Button>
                   {showExamples[index] && (
@@ -54,6 +56,32 @@ function Yalin() {
             </Col>
           ))}
         </Row>
+
+        <div>
+        <div>
+          <h2 className='m-5 yalin-baslik text-center'>MRP(Malzeme İhtiyaç Planlaması )</h2>
+        </div>
+         <Row className='d-flex flex-wrap justify-content-between align-items-center'>
+          <Col sm={12} md={6} >
+            <img className='w-100' src={mrp} alt="" />
+          </Col>
+          <Col sm={12} md={6} className=''> 
+          <p className='mrp-par'>Malzeme İhtiyaç Planlaması (MRP - Material Requirements Planning), bir işletmenin üretim sürecinde ihtiyaç duyduğu malzemeleri planlamak ve doğru zamanda tedarik etmek için kullanılan bir yöntemdir. MRP sistemi, talep edilen ürün miktarına ve üretim planına bağlı olarak, gerekli hammadde ve bileşen miktarını belirleyerek stok ve tedarik sürecini optimize etmeyi hedefler. Bu sayede, işletmeler üretim için ihtiyaç duyulan malzemeleri fazla ya da eksik tedarik etme riskini azaltarak maliyetlerini kontrol altına alabilir ve üretim sürekliliğini sağlar.</p>
+          </Col>
+          <Col sm={12} md={8} className='mt-5'>
+          <h2 className='mrp-altbaslik'>MRP sistemi şu üç temel unsura dayanır:</h2>
+          <ul className='mrp-list'>
+            <li><span>1.Ürün Ağacı (BOM - Bill of Materials): </span>Üretilecek ürünlerin hangi malzemelerden oluştuğunu gösteren bir listedir. Bu liste, ürünlerin tüm bileşenlerini ve alt bileşenlerini içerir.</li>
+            <li><span>2.Ana Üretim Programı (MPS - Master Production Schedule):</span> Üretilmesi planlanan ürünlerin miktar ve üretim zamanlamasını gösterir. Bu program, müşteri taleplerine ve işletmenin üretim kapasitesine göre belirlenir.</li>
+            <li><span>3.Stok Durumu:</span>  Mevcut stok seviyeleri, sipariş durumları ve bekleme süreleri hakkında bilgi içerir. Stok durumları dikkate alınarak, ihtiyaç duyulan malzemeler zamanında sipariş edilir.</li>
+          </ul>
+            
+          </Col>
+          <Col sm={12} md={4}>
+          <img className='w-100' src={mrp2} alt="" />
+          </Col>
+         </Row>
+        </div>
       </Container>
     </>
   );
